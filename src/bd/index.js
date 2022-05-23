d4dfed29329827a58fe1a10bcd920cb4c29216aa
egogoger <bedovegor2203@gmail.com>
2022-05-23 13:49:18 +0300

    feat: refactor bd structure WIP

--- src/bd/index.js
+++ src/bd/index.js
@@ -1,62 +1,59 @@
 const fs = require('fs');
 const path = require('path');
 
-const ORDERS_TO_CANDLES = 'orders_to_candles.json';
+const PATHS = {
+    STORAGE: 'storage',
+    BACKTEST: 'storage/backtest',
+    LIVE: 'storage/live',
+    SANDBOX: 'storage/sandbox',
+    CANDLES: 'storage/candles',
+};
 
 class bd {
     constructor() {
-        this.ensureFile(ORDERS_TO_CANDLES, []);
+        this.ensureStorageDirs();
     }
 
-    getOrdersToCandles() {
-        let data;
-        try {
-            data = JSON.parse(fs.readFileSync(ORDERS_TO_CANDLES, 'utf8'));
-        } catch (e) {
-            console.error(e);
-            data = [];
-        }
-        return data;
+    ensureStorageDirs() {
+        [PATHS.STORAGE, PATHS.BACKTEST, PATHS.LIVE, PATHS.SANDBOX, PATHS.CANDLES]
+            .map(p => path.resolve(__dirname, p))
+            .forEach(this.ensureDir);
     }
 
-    hasOrder(orderId, candleTime) {
-        const ordersToCandles = this.getOrdersToCandles();
-        return Boolean(
-            ordersToCandles.find(
-                orderToCandle => orderToCandle.orderId === orderId && orderToCandle.candleTime === candleTime,
-            ),
-        );
-    }
-
-    saveOrderAndCandle(orderId, candleTime) {
-        const ordersToCandles = JSON.parse(fs.readFileSync(ORDERS_TO_CANDLES, 'utf8'));
-        ordersToCandles.push({ orderId, candleTime });
-        fs.writeFileSync(ORDERS_TO_CANDLES, JSON.stringify(ordersToCandles, null, 4));
-    }
-
-    ensureFile(fileName, defValue) {
-        fs.stat(fileName, function (err, stat) {
-            if (err) {
-                if (err.code === 'ENOENT') {
-                    fs.writeFileSync(fileName, JSON.stringify(defValue, null, 4));
-                    console.log(`${fileName} created`);
-                } else {
-                    console.error('Some other error: ', err.code);
-                }
-            }
-        });
+    ensureDir(path) {
+        if (!fs.existsSync(path)) fs.mkdirSync(path, { recursive: true });
     }
 
     getCachedCandles(fileName) {
-        const pathToFile = path.resolve(__dirname, `${fileName}.json`);
+        const pathToFile = path.resolve(__dirname, `${PATHS.CANDLES}/${fileName}.json`);
 
         if (!fs.existsSync(pathToFile)) return undefined;
 
         return JSON.parse(fs.readFileSync(pathToFile, 'utf8'));
     }
 
-    save(data, fileName) {
-        fs.writeFileSync(path.resolve(__dirname, `${fileName}.json`), JSON.stringify(data, null, 4));
+    saveCandles(data, fileName) {
+        this.save(data, `${PATHS.CANDLES}/${fileName}`);
+    }
+
+    saveBackTest(data, fileName) {
+        this.save(data, `${PATHS.BACKTEST}/${fileName}`);
+    }
+
+    saveSandbox(data, fileName) {
+        this.save(data, `${PATHS.SANDBOX}/${fileName}`);
+    }
+
+    saveLive(data, fileName) {
+        this.save(data, `${PATHS.LIVE}/${fileName}`);
+    }
+
+    save(data, pathToFile) {
+        const finalPath = path.resolve(__dirname, `${pathToFile}.json`);
+
+        this.ensureDir(finalPath.slice(0, finalPath.lastIndexOf('/')));
+
+        fs.writeFileSync(finalPath, JSON.stringify(data, null, 4));
     }
 }
 
